513da635eacd4c999beb52b9d369e46769c064b0
lucas <lucasmelo8328@gmail.com>
2025-06-24 17:22:50 -0300

    feat: estrutura da seção Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex-grow">
-      <h1>Tela Inicial</h1>
+      <div className="container mx-auto">
+        <section>
+          <h1>Receitas Deliciosas</h1>
+          <p>Descubra receitas simples e saboraosas para todas as ocasiões</p>
+          <Link href="/receitas">
+          Ver todas as receitas</Link>
+        </section>
+      </div>
     </main>
   );
 }
