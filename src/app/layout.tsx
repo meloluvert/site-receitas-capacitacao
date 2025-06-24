008f6b78578071a2e14f2a6301403b2d340657ad
lucas <lucasmelo8328@gmail.com>
2025-06-24 16:30:26 -0300

    feat: estrutura do header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Header from "@/components/Header";
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"]
@@ -22,7 +22,9 @@ export default function RootLayout({
       <body
         className={`${inter.variable} antialiased`}
       >
+    <Header/>
         {children}
+        
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Tela Inical</h1>
+      <h1>Tela Inicial</h1>
     </div>
   );
 }
