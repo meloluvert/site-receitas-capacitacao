b1ac5a9445de33e964c249865d9875fa4fbbedeb
lucas <lucasmelo8328@gmail.com>
2025-06-24 22:28:31 -0300

    feat: estrutura da página receitas

---
+++ src/app/receitas/page.tsx
@@ -0,0 +1,12 @@
+export default function ReceitasPage() {
+    return (
+        <main className="flex-grow py-8">
+            <div className="container mx-auto">
+                <h1 className="text-3xl font-bold">Todas as receitas</h1>
+                <div>
+
+                </div>
+            </div>
+        </main>
+    )
+}
