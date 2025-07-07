f4a36ea819927b7c4664e1ac4cd4c4a0c790d9e6
lucas <lucasmelo8328@gmail.com>
2025-07-07 15:39:53 -0300

    feat: busca dados de receita especifica

--- src/app/receitas/[id]/page.tsx
+++ src/app/receitas/[id]/page.tsx
@@ -1,17 +1,46 @@
+"use client";
 import Link from "next/link";
 import { ChevronLeft } from "lucide-react";
 import Image from "next/image";
-import { recipes } from "@/lib/data";
 import { notFound } from "next/navigation";
 import InfoPill from "@/components/InfoPill";
 import PreparationStep from "@/components/PreparationStep";
+import { useEffect, useState } from "react";
+import api from "@/lib/api";
+import { Recipe } from "@/lib/data";
 interface RecipePageProps {
   params: {
     id: string;
   };
 }
 export default function ReceitaPage({ params }: RecipePageProps) {
-  const recipe = recipes.find((recipe) => recipe.id === params.id);
+  const [recipe, setRecipe] = useState<Recipe | null>(null);
+
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      try {
+        const response = await api.get(`/recipes/${params.id}`);
+        setRecipe(response.data);
+      } catch (error) {
+        console.error("Erro ao requisitar receita", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchRecipe();
+  }, []);
+  if (loading) {
+    return (
+      <main className="flex-grow py-8">
+        <div className="container mx-auto mb-8">
+          <div className="flex justify-center">
+            <p>Carregando Receita</p>
+          </div>
+        </div>
+      </main>
+    );
+  }
   if (!recipe) {
     return notFound();
   }
@@ -43,10 +72,10 @@ export default function ReceitaPage({ params }: RecipePageProps) {
             </div>
 
             <div className="flex flex-col sm:flex-row gap-4 justify-center sm:justify-start">
-                <InfoPill title={"Preparo"} info={recipe.prepTime}/>
-                <InfoPill title={"Cozimento"} info={recipe.cookTime}/>
-                <InfoPill title={"Porções"} info={recipe.servings}/>
-                <InfoPill title={"Categoria"} info={recipe.category}/>
+              <InfoPill title={"Preparo"} info={recipe.prepTime} />
+              <InfoPill title={"Cozimento"} info={recipe.cookTime} />
+              <InfoPill title={"Porções"} info={recipe.servings} />
+              <InfoPill title={"Categoria"} info={recipe.category} />
             </div>
 
             <div className="grid sm:grid-cols-2 gap-2">
@@ -54,19 +83,27 @@ export default function ReceitaPage({ params }: RecipePageProps) {
                 <h2 className="text-xl font-bold mb-4">Ingredientes</h2>
                 <ul className="list-disc list-inside space-y-2">
                   {recipe.ingredients.map((ingredient) => (
-                    <li key={ingredient} className="marker:text-orange-500">{ingredient}</li>
+                    <li
+                      key={ingredient.value}
+                      className="marker:text-orange-500"
+                    >
+                      {ingredient.value}
+                    </li>
                   ))}
                 </ul>
               </div>
               <div>
-              <h2 className="text-xl font-bold mb-4">Modo de Preparo</h2>
-              <ol className="space-y-4">
-                {recipe.instructions.map((instruction, index) =>(
-                    <PreparationStep key={index} index={index+1} description={instruction}/>
-                )
-                )}
-              </ol>
-                </div>
+                <h2 className="text-xl font-bold mb-4">Modo de Preparo</h2>
+                <ol className="space-y-4">
+                  {recipe.instructions.map((instruction, index) => (
+                    <PreparationStep
+                      key={instruction.value}
+                      index={index + 1}
+                      description={instruction.value}
+                    />
+                  ))}
+                </ol>
+              </div>
             </div>
           </div>
         </section>
